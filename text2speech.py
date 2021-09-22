589dd36f95abf216676c0ac12a522f2deb70a4db
Sumit <63702667+sumit169-hub@users.noreply.github.com>
2021-09-22 13:45:02 +0530

    Update text2speech.py

--- text2speech.py
+++ text2speech.py
@@ -3,6 +3,6 @@ from playsound import  playsound
 
 audio="speech.mp3"
 language='en'
-sp=gTTS(text='ENTER YOUR TEXT' ,lang=language, slow=False)
+sp=gTTS(text=input('ENTER YOUR TEXT: \n') ,lang=language, slow=False)
 sp.save(audio)
 playsound(audio)
